dc3a75154cecc6821349f02f733d62174fc40176
Drew Stone <drewstone329@gmail.com>
2018-09-18 20:52:50 +0200

    V2 work, separating contracts, adding execution and issuer logic [WIP]

--- contracts/identity/council/V2/IdentityCouncil.sol
+++ contracts/identity/council/V2/IdentityCouncil.sol
@@ -4,64 +4,326 @@ import "zos-lib/contracts/migrations/Initializable.sol";
 import "./ProposalManager.sol";
 
 contract IdentityCouncil is Initializable {
-    ProposalManager manager;
+    ProposalManager PM;
+
+    struct Execution {
+        address to;
+        uint256 value;
+        bytes data;
+        bool executed;
+    }
+
+    uint256 executionNonce;
+    mapping (uint256 => Execution) executions;
 
     mapping (address => uint) public councilIndex;    
     mapping (address => uint) public councilMemberWeight;
+    mapping (address => uint) public issuersIndex;    
+    mapping (address => uint) public issuersWeight;
 
-    // Council address identities, targetting ERC725 identity contracts
+    // Council address identities, targeting ERC725 identity contracts
     address[] council;
 
+    // Claims issuer identities, targeting ERC725 identity contracts
+    address[] issuers;
+
     // Amount of money to deposit when adding a new proposal
+    // Parameter type 1
     uint256 public sybilThresholdValue;
 
     // Percentage of council's votes needed to add new council member
-    uint256 public quorumThreshold;
+    // Parameter type 2
+    uint256 public candidateQuorumThreshold;
 
-    event CouncilSetup(uint256 sybilResistantThresholdValue, uint256 quorumThreshold, address[] council);
-    event CouncilMemberAdded(address member, uint count);
-    event CouncilMemberRemoved(address member, uint count);
+    // Percentage of council's votes needed to add new claims issuer
+    // Parameter type 3
+    uint256 public issuersQuorumThreshold;
+
+    // Percentage of council's votes needed to execute a new execution
+    // Parameter type 4
+    uint256 public executionQuorumThreshold;
+
+    event CouncilSetup(
+        uint256 sybilResistantThresholdValue,
+        uint256[] indexed quorumthresholds,
+        address[] indexed council,
+        address[] indexed issuers);
+
+    event CouncilMemberAdded(
+        address indexed candidate,
+        uint indexed count);
+    event CouncilMemberRemoved(
+        address indexed candidate,
+        uint indexed count);
+
+    event IssuerAdded(
+        address indexed candidate,
+        uint indexed count);
+    event IssuerRemoved(
+        address indexed candidate,
+        uint indexed count);
+
+    event Executed(
+        uint256 indexed executionId,
+        address indexed to,
+        uint256 indexed value,
+        bytes data);
+    event ExecutionFailed(
+        uint256 indexed executionId,
+        address indexed to,
+        uint256 indexed value,
+        bytes data);
 
     function initialize(
         address _proposalManagerAddress,
         uint256 _sybilThresholdValue,
-        uint256 _quorumThreshold,
-        address[] _council
+        uint256 _candidateQuorumThreshold,
+        uint256 _issuersQuorumThreshold,
+        uint256 _executionQuorumThreshold,
+        address[] _council,
+        address[] _issuers
     )
         isInitializer
         public 
     {
+        require ( _candidateQuorumThreshold > 0 && _candidateQuorumThreshold <= 100);
+        require ( _issuersQuorumThreshold > 0 && _issuersQuorumThreshold <= 100);
+        require ( _executionQuorumThreshold > 0 && _executionQuorumThreshold <= 100);
 
+        sybilResistantThresholdValue = _sybilThresholdValue;
+        candidateQuorumThreshold = _candidateQuorumThreshold;
+        issuersQuorumThreshold = _issuersQuorumThreshold;
+        executionQuorumThreshold = _executionQuorumThreshold;
+        PM = ProposalManager(_proposalManagerAddress);
+        
+        council.length++;
+        issuers.length++;
+
+        // Add council members
+        add(msg.sender, 1);
+        for (uint i = 0; i < _council.length; i++) {
+            add(_council[i], 1);
+        }
+
+        // Add claims issuers
+        add(msg.sender, 2);
+        for (uint i = 0; i < _issuers.length; i++) {
+            add(_issuers[i], 2);
+        }
+
+        emit CouncilSetup(
+            sybilResistantThresholdValue,
+            [candidateQuorumThreshold, issuersQuorumThreshold, executionQuorumThreshold],
+            council,
+            issuers);
     }
 
-    function add() isProposalManager public returns (bool) {
+    function add(
+        uint8 _candidateType,
+        address _candidate
+    )
+        isInitialized
+        isProposalManager
+        public
+        returns (bool)
+    {
+        require( _candidate != address(0x0) );
 
+        // Council member addition
+        if (_candidateType == 1) {
+            uint index = council.length++;
+            councilIndex[_candidate] = index;
+            council[index] = _candidate;
+            councilMemberWeight[_candidate] = 1;
+
+            emit CouncilMemberAdded(
+                _candidate,
+                (council.length > 0) ? council.length - 1 : 0);
+
+        // Claims issuer addition
+        } else if (_candidateType == 2) {
+            uint index = issuers.length++;
+            issuersIndex[_candidate] = index;
+            issuers[index] = _candidate;
+            issuersWeight[_candidate] = 1;
+
+            emit IssuerAdded(
+                _candidate,
+                (issuers.length > 0) ? issuers.length - 1 : 0);
+        } else {
+            throw;
+        }
     }
 
-    function remove() isProposalManager public returns (bool) {
+    function remove(
+        uint8 _candidateType,
+        address _candidate
+    )
+        isInitialized
+        isProposalManager
+        public
+        returns (bool)
+    {
+        require( _candidate != address(0x0) );
 
+        // Council member removal
+        if (_candidateType == 1) {
+            uint index = councilIndex[_candidate];
+            delete councilIndex[_candidate];
+            council = resizeArray(index, council);
+
+            emit CouncilMemberRemoved(
+                _candidate,
+                (council.length > 0) ? council.length - 1 : 0);
+
+        // Claims issuer removal
+        } else if (_candidateType == 2) {
+            uint index = issuersIndex[_candidate];
+            delete issuersIndex[_candidate];
+            issuers = resizeArray(index, issuers);
+
+            emit IssuerRemoved(
+                _candidate,
+                (issuers.length > 0) ? issuers.length - 1 : 0);
+        } else {
+            throw;
+        }
     }
 
-    function execute() isProposalManager {
-
+    function update(
+        uint8 _parameterType,
+    )
+        returns
+    {
+        if (_paramterType == 1) {
+            quorumThreshold = IC.sybilThresholdValue;
+        } else if (_paramterType == 2) {
+            quorumThreshold = IC.candidateQuorumThreshold;
+        } else if (_paramterType == 3) {
+            quorumThreshold = IC.issuersQuorumThreshold;
+        } else if (_paramterType == 4) {
+            quorumThreshold = IC.executionQuorumThreshold
+        } else {
+            throw;
+        }
     }
     
-    function resizeCouncilArray(uint index) internal {
-        address[] memory arrayNew = new address[](council.length-1);
-        for (uint i = 0; i<arrayNew.length; i++){
-            if(i != index && i<index){
-                arrayNew[i] = council[i];
+
+    // TODO: Check if this logic is sound. We want to allow
+    //       a new execution to take place from the ProposalManager
+    function execute(
+        address _to,
+        bytes _data,
+        uint _value
+    )
+        isInitialized
+        isProposalManager
+        public
+        returns (bool)
+    {
+        require( executions[executionNonce].to == address(0x0) );
+
+        executions[executionNonce].to = _to;
+        executions[executionNonce].value = _value;
+        executions[executionNonce].data = _data;
+
+        success = executions[executionNonce].to.call(executions[executionNonce].data, 0);
+        if (success) {
+            executions[executionNonce].executed = true;
+            emit Executed(
+                executionNonce,
+                executions[executionNonce].to,
+                executions[executionNonce].value,
+                executions[executionNonce].data);
+        } else {
+            emit ExecutionFailed(
+                executionNonce,
+                executions[executionNonce].to,
+                executions[executionNonce].value,
+                executions[executionNonce].data
+            );
+        }
+
+        executionNonce++;
+    }
+
+    // TODO: Check if this logic is sound. We want to allow
+    //       any approved execution to be re-executed by anyone.
+    function reExecute(
+        uint256 _id
+    )
+        isInitialized
+        public
+    {
+        require( !executions[_id].executed );
+        success = executions[_id].to.call(executions[_id].data, 0);
+        if (success) {
+            executions[_id].executed = true;
+            emit Executed(
+                _id,
+                executions[_id].to,
+                executions[_id].value,
+                executions[_id].data);
+        } else {
+            emit ExecutionFailed(
+                _id,
+                executions[_id].to,
+                executions[_id].value,
+                executions[_id].data
+            );
+        }
+
+        executionNonce++;
+        return success;
+    }
+
+    function resizeArray(
+        uint index,
+        address[] _array
+    )
+        internal
+        constant
+        returns (address[]);
+    {
+        address[] memory arrayNew = new address[](length - 1);
+        for (uint i = 0; i < arrayNew.length; i++){
+            if (i != index && i < index){
+                arrayNew[i] = _array[i];
             } else {
-                arrayNew[i] = council[i+1];
+                arrayNew[i] = _array[i + 1];
             }
         }
-        delete council;
-        council = arrayNew;
+
+        return arrayNew;
+    }
+
+    function getQuorumThreshold(
+        uint _parameterType
+    )
+        public
+        constant
+        returns (uint)
+    {
+        if (_paramterType == 1) {
+            quorumThreshold = IC.sybilThresholdValue;
+        } else if (_paramterType == 2) {
+            quorumThreshold = IC.candidateQuorumThreshold;
+        } else if (_paramterType == 3) {
+            quorumThreshold = IC.issuersQuorumThreshold;
+        } else if (_paramterType == 4) {
+            quorumThreshold = IC.executionQuorumThreshold
+        } else {
+            throw;
+        }
     }
     
     modifier isProposalManager() { 
         require (msg.sender == address(manager)); 
         _; 
     }
-    
+
+    modifier isInitialized() { 
+        require (initialized); 
+        _; 
+    }
 }
--- contracts/identity/council/V2/ProposalManager.sol
+++ contracts/identity/council/V2/ProposalManager.sol
@@ -8,7 +8,7 @@ contract ProposalManager is Initializable {
     IdentityCouncil IC;
 
     bytes32 constant CANDIDATE_PREFIX   = "CANDIDATE";
-    bytes32 constant ACTION_PREFIX      = "ACTION";
+    bytes32 constant EXECUTION_PREFIX   = "EXECUTION";
 
     struct CandidateProposal {
         address candidate;
@@ -16,7 +16,7 @@ contract ProposalManager is Initializable {
         bool voteType; // true for add, false for remove
     }
 
-    struct ActionProposal {
+    struct ExecutionProposal {
         address to;
         bytes data;
         uint value;
@@ -24,82 +24,180 @@ contract ProposalManager is Initializable {
     }
 
     mapping (bytes32 => CandidateProposal) candidateProposals;
-    mapping (bytes32 => ActionProposal) actionProposals;
+    mapping (bytes32 => ExecutionProposal) executionProposals;
 
     mapping (address => mapping (bytes32 => bool)) public votedOnProposal;
     mapping (bytes32 => bool) public proposalExists;
 
-    event CandidateProposalCreated(address candidate, bytes32 proposalKey);
-    event CandidateProposalVotedOn(address candidate, bytes32 proposalKey);
+    event ProposalManagerSetup(address identityCouncil);
 
-    event ActionProposalCreated(address sender, bytes32 proposalKey);
-    event ActionProposalVotedOn(address sender, bytes32 proposalKey);
+    event CandidateProposalCreated(
+        address candidate,
+        bytes32 proposalKey);
+    event CandidateProposalVotedOn(
+        address candidate,
+        bytes32 proposalKey);
+
+    event ExecutionProposalCreated(
+        address indexed to,
+        bytes data,
+        uint indexed value,
+        bytes32 indexed proposalKey);
+    event ExecutionProposalVotedOn(
+        address indexed to,
+        bytes data,
+        uint indexed value,
+        bytes32 indexed proposalKey);
 
     function initialize(address _identityCouncilAddress) isInitializer public {
         IC = IdentityCouncil(_identityCouncil);
     }
 
-    function proposeCandidate(address _candidate, bool _voteType) public {
+    function proposeCandidate(
+        address _candidate,
+        bool _voteType
+    )
+        isInitialized
+        public
+    {
         require( _candidate != address(0x0) );
+
+        bytes32 _key = keccak256(abi.encodePacked(CANDIDATE_PREFIX, _candidate));
+        require( !proposalExists[_key] );
+        proposalExists[_key] = true;
+
+        candidateProposals[_key] = CandidateProposal({
+            candidate: _candidate,
+            voteCount: (IC.councilIndex[msg.sender] > 0) ? 1 : 0,
+            voteType: _voteType
+        });
+
+        votedOnProposal[msg.sender][_key] = true;
     }
 
-    function proposeAction(address _to, bytes _data, uint _value) public {
+    function proposeExecution(
+        address _to,
+        bytes _data,
+        uint _value
+    )
+        isCouncilMember
+        isInitialized
+        public
+    {
         require( _to != address(0x0) );
+        require( _data != bytes(0x0) );
+
+        bytes32 _key = keccak256(abi.encodePacked(EXECUTION_PREFIX, _to, _data, _value));
+        require( !proposalExists[_key] );
+        proposalExists[_key] = true;
+
+        executionProposals[_key] = ExecutionProposal({
+            to: _to,
+            data: _data,
+            value: _value,
+            voteCount: 1,
+            approved: false
+        });
+
+        votedOnProposal[msg.sender][_key] = true;
     }
 
-    function vote(uint8 _proposalType, bytes32 _proposalKey) isCouncilMember public {
+    function vote(
+        uint8 _parameterType,
+        bytes32 _proposalKey
+    )
+        isCouncilMember
+        isInitialized
+        public
+    {
         require( _proposalKey != bytes32(0x0) );
+        require( proposalExists[_proposalKey] );
         require( !votedOnProposal[msg.sender][_proposalKey] );
 
-        bool success;
-        if (_proposalType == 0) {
+        if (_parameterType == 2) {
             require( candidateProposals[_proposalKey].candidate != address(0x0) );
-            success = voteOnCandidate(_proposalKey);
-        } else if (_proposalType == 1) {
+            success = voteOnCandidate(_proposalKey, msg.sender);
+        } else if (_parameterType == 4) {
             require( actionProposals[_proposalKey].to != address(0x0) );
-            success = voteOnAction(_proposalKey);
+            success = voteOnExecution(_proposalKey, msg.sender);
         } else {
             throw;
         }
 
-        require( success );
         votedOnProposal[msg.sender] = true;
     }
 
-    function voteOnCandidate(bytes32 _proposalKey) internal returns (bool) {
-        require( !votedOnProposal[msg.sender][proposalIndex] );
-
+    function voteOnCandidate(
+        bytes32 _proposalKey,
+        address sender
+    )
+        internal
+    {
         CandidateProposal memory p = candidateProposals[_proposalKey];
 
         p.voteCount += councilMemberWeight[sender];
 
-        if (isVoteSuccess(p.voteCount)) {
+        if (isVoteSuccess(p.voteCount, 2)) {
             bool success = (p.voteType)
-                ? council.add(p.candidate)
-                : council.remove(p.candidate);
+                ? IC.add(p.candidate)
+                : IC.remove(p.candidate);
 
             require( success );
 
-            delete candidateProposals[proposalIndex];
+            delete candidateProposals[_proposalKey];
             delete candidateProposalExists[p.candidate];
         }
 
         emit CandidateProposalVotedOn(p.candidate, p.voteCount, p.voteType);
     }
+
+    function voteOnExecution(
+        bytes32 _proposalKey,
+        address sender
+    )
+        internal
+    {
+        ExecutionProposal memory p = executionProposals[_proposalKey];
+
+        p.voteCount += councilMemberWeight[sender];
+
+        if (isVoteSuccess(p.voteCount, 4)) {
+            IC.execute(p.to, p.data, p.value);
+
+            delete executionProposals[_proposalKey];
+            delete executionProposalExists[p.candidate];
+        }
+
+        emit ExecutionProposalVotedOn(p.candidate, p.voteCount, p.voteType);
+    }
     
-    function isVoteSuccess(uint count) internal constant returns (bool) {
+    function isVoteSuccess(
+        uint _count,
+        uint8 _parameterType
+    )
+        internal
+        constant
+        returns (bool)
+    {
+        uint8 quorumThreshold = IC.getQuorumThreshold(_parameterType);
+
         // Calculate fraction of votes = voteCount / council.length
-        uint frac = SafeMath.div(SafeMath.mul(1 ether, count), council.getCouncilSize());
+        uint frac = SafeMath.div(SafeMath.mul(1 ether, _count), IC.getCouncilSize());
 
         // Calculate quorum threshold = quorumThreshold / 100
-        uint thresh = SafeMath.div(SafeMath.mul(1 ether, council.quorumThreshold), 100);
+        uint thresh = SafeMath.div(SafeMath.mul(1 ether, quorumThreshold), 100);
 
         // Check if fraction of votes is larger than quorum threshold
         return frac >= thresh;
     }
 
     modifier isCouncilMember() { 
-        require (council.councilIndex[msg.sender] > 0); 
+        require (IC.councilIndex[msg.sender] > 0); 
+        _; 
+    }
+
+    modifier isInitialized() { 
+        require (initialized); 
         _; 
     }
 }
