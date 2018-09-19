d63aabcdc5dd55be1d226706e560c8f72fdb4f68
Drew Stone <drewstone329@gmail.com>
2018-09-19 15:22:17 +0200

    Adding licenses

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,3 +1,22 @@
+/*    
+    copyright 2018 to the Commonwealth-HQ Authors
+
+    This file is part of Commonwealth-HQ.
+
+    Commonwealth-HQ is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Commonwealth-HQ is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Commonwealth-HQ.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 const KeyHolderLibrary = artifacts.require("./KeyHolderLibrary.sol");
 const ClaimHolderLibrary = artifacts.require("./ClaimHolderLibrary.sol");
 const CommonwealthIdentity = artifacts.require("./CommonwealthIdentity.sol");
