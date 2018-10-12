f5730a989cd56aaeee87252fd778df99d94c3ace
Drew Stone <drewstone329@gmail.com>
2018-10-12 21:26:25 +0300

    New math

--- contracts/mechanisms/LockDrop.sol
+++ contracts/mechanisms/LockDrop.sol
@@ -13,7 +13,9 @@ import "../token/EdgewareERC20.sol";
  * to the chosen locking schedule.
  */
 contract LockDrop {
-    uint public capacity;
+    uint public tokenCapcity;
+    uint public totalDeposits;
+    uint public totalEffectiveDeposits;
     uint public beginning;
     uint public ending;
     uint public maxLength;
@@ -25,14 +27,14 @@ contract LockDrop {
 
     mapping (address => Lock[]) locks;
 
-    event Deposit(address sender, uint value, uint length, uint ending);
+    event Deposit(address sender, uint value, uint effectiveValue, uint ending);
     event Unlock(address sender, uint value);
-    event Withdraw(address sender, uint value);
+    event Withdraw(address sender, uint effectiveValue);
 
-    constructor(uint _lockPeriod, uint _maxLength, uint _capacity) public {
+    constructor(uint _lockPeriod, uint _maxLength, uint _tokenCapacity) public {
         beginning = now;
         ending = now + (1 days * _lockPeriod);
-        capacity = _capacity;
+        tokenCapacity = _tokenCapacity;
         maxLength = _maxLength;
     }
 
@@ -41,16 +43,21 @@ contract LockDrop {
      * @param      _length  The length of time chosen for locking ether
      */
     function lock(uint _length) payable public {
-        require(hasNotEnded());
-        require(_length <= maxLength);
-        require(msg.value <= capacity && msg.value > 0);
-        capacity -= msg.value;
+        require( hasNotEnded() );
+        require( _length <= maxLength );
+        require( msg.value > 0 );
+        require( isValidLength(_length) );
+
+        effectiveAmount = effectiveDepositValue(msg.value, _length);
+        totalDeposits += msg.value;
+        totalEffectiveDeposits += effectiveAmount;
 
         // Create deposit with paid amount and specified length
         // of time. The lock ending is determined as the specified
         // time length after the ending of the lock up period in days.
         Lock memory l = Lock({
             amount: msg.value,
+            effectiveAmount: effectiveAmount,
             lockEnding: ending + (_length * 1 days)
         });
 
@@ -58,7 +65,7 @@ contract LockDrop {
         locks[msg.sender].push(l);
 
         // Emit Deposit event
-        emit Deposit(msg.sender, msg.value, _length, l.lockEnding);
+        emit Deposit(msg.sender, l.amount, l.effectiveAmount, l.lockEnding);
     }
     
     /**
@@ -84,10 +91,11 @@ contract LockDrop {
      * @dev        Withdraw function should withdraw all valid ether after lock
      */
     function withdraw() public {
-        require(hasEnded());
-        require(locks[msg.sender].length > 0);
+        require( hasEnded() );
+        require( locks[msg.sender].length > 0 );
 
         uint amount = 0;
+        uint effectiveAmount = 0;
 
         // Iterate over all locks
         for (uint i = 0; i < locks[msg.sender].length; i++) {
@@ -96,14 +104,52 @@ contract LockDrop {
             // Aggregate deposit if ending has passed and lockEnding is valid
             if (now >= curr.lockEnding && curr.lockEnding != 0) {
                 amount = SafeMath.add(amount, curr.amount);
+                effectiveAmount = SafeMath.add(effectiveAmount, curr.effectiveAmount);
                 delete locks[msg.sender][i];
             }
         }
 
         msg.sender.transfer(amount);
 
+        uint allocation = (effectiveAmount * tokenCapacity) / totalEffectiveDeposits;
+        EdgewareERC20.mint(msg.sender, allocation);
+
         emit Withdraw(msg.sender, amount);
     }
+
+    function effectiveDepositValue(uint value, uint length) internal constant returns (uint) {
+        uint effectiveValue = 0;
+
+        // TODO: Ensure units are safe to use
+        if (length == 91 days) {
+            effectiveValue = value;
+        } else if (length == 182 days) {
+            effectiveValue = (value * 3.75 ether) / 100 ether;
+        } else if (length == 365 days) {
+            effectiveValue = (value * 7.5 ether) / 100 ether;
+        } else if (length == 730 days) {
+            effectiveValue = (value * 15 ether) / 100 ether;
+        } else if (length == 1095 days) {
+            effectiveValue = (value * 15 ether) / 100 ether;
+        } else {
+            revert();
+        }
+    }
+
+    function isValidLength(uint length) internal constant returns (bool) {
+        if (length == 91 days) {
+            continue;
+        } else if (length == 182 days) {
+            continue;
+        } else if (length == 365 days) {
+            continue;
+        } else if (length == 730 days) {
+            continue;
+        } else {
+            revert();
+        }
+    }
+    
 
     function hasNotEnded() public constant returns (bool) {
         return now <= ending;
