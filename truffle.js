79c4345c98f824b49e5b93db2f6bebeac642596f
Drew Stone <drewstone329@gmail.com>
2018-12-26 20:02:26 +0200

    Updates

--- truffle.js
+++ truffle.js
@@ -1,28 +1,38 @@
-require('babel-register');
-require('babel-polyfill');
-
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
-
+require('dotenv').config();
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const INFURA_API_KEY = process.env.INFURA_API_KEY
+const mnemonic = process.env.MNEMONIC;
+console.log(mnemonic);
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-  networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "*" // Match any network id
+    solc: {
+        optimizer: {
+                  enabled: true,
+                  runs: 200
+
+        }
+
+    },
+    networks: {
+        development: {
+                  host: "localhost",
+                  port: 8545,
+                  network_id: "*", // Match any network id
+                  gas: 4700000,
+
+        },
+        ropsten:  {
+                  network_id: 3,
+                  provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + INFURA_API_KEY),
+                  gas: 4500000,
+
+        },
+        rinkeby: {
+                  network_id: "4",
+                  provider: new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/" + INFURA_API_KEY),
+                  gas: 6721975,
+
+        }
+
     }
-  }
+
 };
