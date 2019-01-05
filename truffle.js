ccf7f2d71bb910f33955ee0610e02965a819b1cb
filiplazovic <lazoviccorp@gmail.com>
2019-01-05 23:16:59 +0100

    Uncommented config lines

--- truffle.js
+++ truffle.js
@@ -1,10 +1,10 @@
 require('babel-register');
 require('babel-polyfill');
-// require('dotenv').config();
-// const HDWalletProvider = require("truffle-hdwallet-provider");
-// const INFURA_API_KEY = process.env.INFURA_API_KEY
-// const mnemonic = process.env.MNEMONIC;
-// console.log(mnemonic);
+require('dotenv').config();
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const INFURA_API_KEY = process.env.INFURA_API_KEY
+const mnemonic = process.env.MNEMONIC;
+console.log(mnemonic);
 module.exports = {
   compilers: {
     solc: {
@@ -13,7 +13,7 @@ module.exports = {
         enabled: true,
         runs: 200
       }
-    }  
+    }
   },
   mocha: {
     reporter: 'eth-gas-reporter',
@@ -23,21 +23,21 @@ module.exports = {
     }
   },
   networks: {
-    // development: {
-    //   host: "localhost",
-    //   port: 8545,
-    //   network_id: "*", // Match any network id
-    //   gas: 4700000,
-    // },
-    // ropsten:  {
-    //   network_id: 3,
-    //   provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + INFURA_API_KEY),
-    //   gas: 4500000,
-    // },
-    // rinkeby: {
-    //   network_id: "4",
-    //   provider: new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/" + INFURA_API_KEY),
-    //   gas: 6721975,
-    // }
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*", // Match any network id
+      gas: 4700000,
+    },
+    ropsten:  {
+      network_id: 3,
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + INFURA_API_KEY),
+      gas: 4500000,
+    },
+    rinkeby: {
+      network_id: "4",
+      provider: new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/" + INFURA_API_KEY),
+      gas: 6721975,
+    }
   }
 };
